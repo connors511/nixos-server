f7ee48d9d6538a5e65a695cb67aaae8b3342faee
Matthias Larsen <matthias@msalconsulting.dk>
2026-01-05 08:13:06 +0100

    chore: more move

---
+++ home/edgar/global/default.nix
@@ -0,0 +1,67 @@
+{
+  config,
+  inputs,
+  pkgs,
+  outputs,
+  ...
+}: {
+  imports =
+    [
+      inputs.nix-colors.homeManagerModules.default
+
+#      ./atuin.nix
+      ./bash.nix
+      ./bat.nix
+      ./bottom.nix
+#      ./calendar.nix
+      ./direnv.nix
+      ./eza.nix
+      ./fish.nix
+      ./font.nix
+      ./fzf.nix
+      ./git.nix
+      ./helix
+      ./jq.nix
+      ./kitty.nix
+      ./mime.nix
+      ./nix-index.nix
+      ./rclone.nix
+      ./starship.nix
+      ./xdg.nix
+      ./yazi.nix
+      ./zoxide.nix
+    ]
+    ++ (builtins.attrValues outputs.homeManagerModules);
+
+  colorScheme = inputs.nix-colors.colorSchemes.selenized-light;
+
+  home = {
+    username = "mlarsen";
+    homeDirectory = "/home/${config.home.username}";
+    stateVersion = "22.11";
+  };
+
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  home.packages = with pkgs.stable; [
+    alejandra
+    comma
+    pkgs.csvlens # TODO: move to stable after NixOS 24.05
+    duf
+    du-dust
+    fd
+    ffmpeg
+    hexyl
+    nix-init
+    kondo
+    p7zip
+    ripgrep
+    rnr
+    sshfs
+    pkgs.tcount
+    unsilence
+  ];
+}
---
+++ home/edgar/global/fzf.nix
@@ -0,0 +1,14 @@
+{config, ...}: {
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+
+    defaultOptions = with config.colorscheme.palette; [
+      # https://github.com/sagikazarmark/nix-config/blob/f1380179eae6082e6383fe071982c458041cda85/modules/home-manager/modules/nix-colors/programs/fzf.nix
+      "--color=bg+:#${base01},bg:#${base00},spinner:#${base0C},hl:#${base0D}"
+      "--color=fg:#${base04},header:#${base0D},info:#${base0A},pointer:#${base0C}"
+      "--color=marker:#${base0C},fg+:#${base06},prompt:#${base0A},hl+:#${base0D}"
+    ];
+  };
+}
