49103e85e3a3713c4ec1cda1a93497a67b11eb0a
Matthias Larsen <matthias@msalconsulting.dk>
2026-01-12 09:04:03 +0100

    feat: proxies

--- hosts/svr1/proxies.nix
+++ hosts/svr1/proxies.nix
@@ -1,31 +1,51 @@
 {
-  config,
-  pkgs,
-  services,
-  ...
-}: {
+config,
+...
+}
+:let
+  # Internal services (*.lan) - no SSL, no auth
+  internalProxy = port: {
+    locations."/".proxyPass = "http://127.0.0.1:${toString port}";
+  };
 
-#    services.nginx.virtualHosts."n8n.mlad.dk" = {
-#        locations."/" = {
-##          proxyPass = "http://n8n.coolify.home";
-#
-#            proxyPass = "http://coolify.home";
-#            proxyWebsockets = true;
-##            extraConfig = ''
-##              proxy_set_header Host $host;
-##              proxy_set_header X-Real-IP $remote_addr;
-##              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-##              proxy_set_header X-Forwarded-Proto $scheme;
-##              proxy_set_header X-Forwarded-Host $host;
-##              proxy_set_header X-Forwarded-Port $server_port;
-##              proxy_redirect off;
-##              proxy_buffering off;
-##              proxy_http_version 1.1;
-##              proxy_set_header Connection "";
-##
-##              access_log /var/log/nginx/n8n_access.log combined;
-##              error_log /var/log/nginx/n8n_error.log debug;
-##            '';
-#        };
-#    };
+  internalProxyRemote = host: port: {
+    locations."/".proxyPass = "http://${host}:${toString port}";
+  };
+
+  # External services (*.mlad.dk) - with Authelia
+  externalProxy = port: {
+    enableAuthelia = true;
+#    forceSSL = true;
+    useACMEHost = "mlad.dk";  # or however you handle certs
+    locations."/".proxyPass = "http://127.0.0.1:${toString port}";
+  };
+
+  # External without Authelia (Jellyfin, Jellyseerr - own auth)
+  externalProxyPublic = host: port: {
+#    forceSSL = true;
+    useACMEHost = "mlad.dk";
+    locations."/".proxyPass = "http://${host}:${toString port}";
+  };
+
+in {
+  services.nginx.virtualHosts = {
+    # Internal (.lan) - proxied to local services
+    "home.lan"      = internalProxy 8082;  # Homepage
+    "sonarr.lan"    = internalProxyRemote "homeassistant" 8989;
+    "radarr.lan"    = internalProxyRemote "homeassistant" 7878;
+    "lidarr.lan"    = internalProxyRemote "homeassistant" 8686;
+    "prowlarr.lan"  = internalProxyRemote "homeassistant" 9696;
+    "navidrome.lan"  = internalProxyRemote "homeassistant" 4533;
+
+    # Internal - proxied to HA
+    "jellyfin.lan"  = internalProxyRemote "homeassistant" 8096;
+    "requests.lan"  = internalProxyRemote "homeassistant" 5055;
+
+    # External with Authelia
+#    "grafana.mlad.dk" = externalProxy 3001;
+
+    # External public (own auth)
+    "jellyfin.mlad.dk"  = externalProxyPublic "homeassistant" 8096;
+    "requests.mlad.dk"  = externalProxyPublic "homeassistant" 5055;
+  };
 }
